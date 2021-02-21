e27bed935b38c56c0449318f866b257c4cb263f3
krav maguy <flex4lease@gmail.com>
2021-02-21 04:21:13 -0600

    save first document with code zero

--- server.js
+++ server.js
@@ -1,24 +1,51 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
 const app = express();
-
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+mongoose.connect("mongodb://localhost:27017/urlsToShorten", {
+  useNewUrlParser: true,
+});
 // Basic Configuration
 const port = process.env.PORT || 3000;
-
 app.use(cors());
-
-app.use('/public', express.static(`${process.cwd()}/public`));
-
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use("/public", express.static(`${process.cwd()}/public`));
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
-// Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+const urlSchema = {
+  name: String,
+  code: Number,
+};
+const ShortenedUrl = mongoose.model("ShortenedUrl", urlSchema);
+
+app.post("/api/shorturl/new", function (req, res) {
+  url = req.body.url;
+  ShortenedUrl.find({}, function (err, foundItems) {
+    if (foundItems.length === 0) {
+      console.log("founditems is zero");
+      const dbUrl = new ShortenedUrl({
+        name: url,
+        code: 0,
+      });
+      dbUrl.save(function (err) {
+        if (err) return console.log(err, "there was an error saving it");
+        // saved!
+        console.log("saved successfully first one code zero");
+      });
+    } else {
+      // ShortenedUrl.sort({ code: 'asc'});
+      console.log('there is already items in collection')
+
+    }
+  });
+  res.json({ url });
+
 });
 
-app.listen(port, function() {
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
